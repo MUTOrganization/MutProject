8d99b7885bafa741bd87970d8501765305a700bf
YESTERD4Y <165787169+YESTERD4Y@users.noreply.github.com>
2025-05-18 19:30:23 +0700

    Update Page404.jsx

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,9 +1,41 @@
+import { Button } from "@heroui/react";
+import { useNavigate } from "react-router-dom";
+import { AlertCircle, PackageOpen } from "lucide-react";
+import { useEffect, useState } from "react";
+
 export default function Page404() {
-    return (
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold">404</h1>
-        <p className="text-2xl">ไม่พบหน้าที่คุณกำลังค้นหา</p>
-      </div>
-    );
-  }
-  
+  const navigate = useNavigate();
+  const [counter, setCounter] = useState(10);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (counter === 0) {
+      navigate('/');
+    }
+  }, [counter]);
+
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-50">
+      <PackageOpen className="w-20 h-20 text-red-500 m-4" />
+      <h1 className="text-6xl font-bold text-red-500 mb-2">404</h1>
+      <p className="text-xl text-gray-600 mb-8">ไม่พบหน้าที่คุณกำลังค้นหา</p>
+      <p className="text-xl text-red-500 font-bold mb-8">กำลังนำท่านกลับหน้าหลักใน {counter} วินาที</p>
+      <Button
+        color="primary"
+        variant="solid"
+        className="text-sm font-medium shadow-md hover:scale-105 transition-transform"
+        onPress={() => navigate('/')}
+      >
+        กลับหน้าหลัก
+      </Button>
+    </div>
+  );
+}
