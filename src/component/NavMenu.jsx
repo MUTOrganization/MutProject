42ea892c7044112be0159cdd2bf153170e68a24d
Nattawut2324 <natwtlim123@gmail.com>
2025-05-18 19:37:01 +0700

    แก้ navbar ไม่ปิดเวลาเลือก

--- src/component/NavMenu.jsx
+++ src/component/NavMenu.jsx
@@ -4,50 +4,19 @@ import { useNavigate } from "react-router-dom";
 import { useAppContext } from "../contexts/AppContext";
 import { MainMenu } from "./Sidebar";
 
-function NavMenu() {
-  const [isHovered, setIsHovered] = useState(true);
-  const [isSmallScreen, setIsSmallScreen] = useState(false);
-  const [isFixed, setIsFixed] = useState(
-    Boolean(localStorage.getItem("navbarToggle"))
-  );
+function NavMenu({ onSelectMenu = () => {} }) {
   const {logout } = useAppContext();
 
   const navigate = useNavigate();
-
-
-  const handleToggleSidebar = () => {
-    const _value = !isFixed;
-    setIsFixed(_value);
-    localStorage.setItem("navbarToggle", _value);
-  };
 
   const handleLogout = () => {
     logout();
 
     navigate("/login");
   };
-
-  useEffect(() => {
-    // Define the media query
-    const mediaQuery = window.matchMedia("(max-width: 600px)");
-
-    // Update state based on the media query match
-    setIsSmallScreen(mediaQuery.matches);
-
-    // Define the listener function
-    const handleWindowResize = () => {
-      setIsSmallScreen(mediaQuery.matches);
-    };
-
-    // Add event listener to listen for window resize
-    mediaQuery.addEventListener("change", handleWindowResize);
-
-    // Clean up the event listener on component unmount
-    return () => mediaQuery.removeEventListener("change", handleWindowResize);
-  }, []);
   return (
     <div>
-      <MainMenu isShow={true} isNavMenu={true} />
+      <MainMenu isShow={true} isNavMenu={true} onSelectMenu={onSelectMenu} />
       <div className="sticky bottom-0">
         <Divider className="my-4" />
         <div className="flex justify-center items-center text-small">
