d0d3e36da60cc0ffbd4963ced3f88e7ebc8ef7bf
BeerPrutipong <pottipongjindapang@gmail.com>
2025-05-25 21:54:28 +0700

    Update

--- src/pages/DashboardCEO/Components/AllSummary.jsx
+++ src/pages/DashboardCEO/Components/AllSummary.jsx
@@ -19,9 +19,9 @@ function AllSummary({ expensesData, commissionData, isLoading }) {
     const totalSales = commissionData.reduce((sum, item) => sum + item.data.reduce((acc, item) => acc + item?.adminIncome, 0), 0)
 
     return (
-        <div className='w-full flex flex-row justify-start items-center space-x-5'>
+        <div className='w-full flex flex-row justify-start items-center space-x-4'>
 
-            <div className='bg-white rounded-lg py-2 px-6 flex flex-row justify-between items-center space-x-8 relative h-28 border-3 border-slate-50'>
+            <div className='bg-white rounded-lg py-2 px-6 w-full flex flex-row justify-between items-center space-x-8 relative h-28 border-8 border-slate-50'>
                 <div className='subBox1'>
                     <header className='text-start text-sm text-slate-500 mb-3 absolute top-4'>กำไรสุทธิ</header>
                     <div className='mt-3'>
@@ -34,26 +34,26 @@ function AllSummary({ expensesData, commissionData, isLoading }) {
                 <div className='subBox2'>
                     <header className='text-start text-sm text-slate-500 mb-3 absolute top-4'>ยอดขาย</header>
                     <div className='mt-3'>
-                        <div className='text-2xl'>{isLoading || commissionData.length === 0 ? <Spinner variant='gradient' size='sm' color='danger' /> : formatNumber(totalSales)}</div>
+                        <div className='text-2xl'>{isLoading || commissionData.length === 0 ? <div className='w-28'><Spinner variant='gradient' size='sm' color='primary' /></div> : formatNumber(totalSales)}</div>
                     </div>
                 </div>
 
                 <div className='h-20 w-0.5 bg-slate-100 rounded-md'></div>
 
-                <div className='subBox1'>
+                <div className='subBox1 w-28'>
                     <header className='text-start text-sm text-slate-500 mb-3 absolute top-4'>ค่าใช้จ่ายรวม</header>
                     <div className='mt-4 flex flex-col justify-center items-start py-4'>
-                        <div className='text-2xl'>{commissionData.length === 0 ? <Spinner variant='gradient' size='sm' color='danger' /> : formatNumber(expensesNetAmount)}</div>
+                        <div className='text-2xl'>{isLoading || commissionData.length === 0 ? <div className='w-28'><Spinner variant='gradient' size='sm' color='primary' /></div> : formatNumber(expensesNetAmount)}</div>
                         <div className='text-sm text-slate-500'></div>
                     </div>
                 </div>
             </div>
 
-            <div className='bg-white rounded-lg py-3 px-6 h-28 flex flex-row justify-between items-center space-x-8 relative  border-3 border-slate-50'>
+            <div className='bg-white rounded-lg py-3 px-6 h-28 flex flex-row justify-between items-center space-x-8 relative w-full border-8 border-slate-50'>
                 <div className='subBox2 px-4'>
                     <header className='text-start text-sm text-slate-500 mb-3 absolute top-4'>ค่าคอมมิชชั่น</header>
                     <div className='flex flex-col justify-center items-start mt-8'>
-                        <div className='text-slate-500 text-2xl'>{isLoading || commissionData.length === 0 ? <Spinner variant='gradient' size='sm' color='danger' /> : formatNumber(totalCommission)}</div>
+                        <div className='text-slate-500 text-2xl'>{isLoading || commissionData.length === 0 ? <div className='w-28'><Spinner variant='gradient' size='sm' color='primary' /></div> : formatNumber(totalCommission)}</div>
 
                         <div className='flex flex-row justify-between items-center space-x-4 w-full my-1'>
                             <div className='text-orange-500 text-sm'>
@@ -68,7 +68,7 @@ function AllSummary({ expensesData, commissionData, isLoading }) {
                 <div className='subBox2 px-4'>
                     <header className='text-start text-sm text-slate-500 mb-3 absolute top-4'>ค่าใช้จ่ายอื่นๆ</header>
                     <div className='flex flex-col justify-center items-start mt-8'>
-                        <div className='text-slate-500 text-2xl'>{isLoading ? <Spinner color='danger' variant='gradient' size='sm' /> : formatNumber(totalExpenses)}</div>
+                        <div className='text-slate-500 text-2xl'>{isLoading ? <div className='w-28'><Spinner variant='gradient' size='sm' color='primary' /></div> : formatNumber(totalExpenses)}</div>
 
                         <div className='flex flex-row justify-between items-center space-x-4 w-full my-1'>
                             <div className='text-orange-500 text-sm'>
@@ -78,7 +78,7 @@ function AllSummary({ expensesData, commissionData, isLoading }) {
                     </div>
                 </div>
             </div>
-            <div className='bg-white rounded-lg px-4 py-5 h-28 flex flex-col justify-center items-center space-y-2 w-full  border-3 border-slate-50'>
+            <div className='bg-white rounded-lg px-4 py-5 h-28 flex flex-col justify-center items-center space-y-2 w-full  border-8 border-slate-50'>
                 <div className='flex flex-row justify-between items-center space-x-4 w-full'>
                     <span className='text-lg text-slate-500'>ลูกค้าใหม่</span>
                     <span className='text-green-500 font-semibold'>500</span>
@@ -90,10 +90,10 @@ function AllSummary({ expensesData, commissionData, isLoading }) {
                 </div>
             </div>
 
-            <div className='bg-white rounded-lg px-4 py-5 h-28 w-full relative  border-3 border-slate-50'>
-                <header className='text-start text-lg text-slate-500 mb-3 absolute top-4'>ออเดอร์</header>
+            <div className='bg-white rounded-xl px-4 text-slate-500 py-5 h-28 w-full relative border-8 border-slate-50'>
+                <header className='text-start text-lg mb-3 absolute top-4'>ออเดอร์</header>
                 <div className='mt-4 flex flex-row justify-center items-start py-4'>
-                    <div className='text-2xl'>{commissionData.length === 0 ? '' : commissionData.reduce((sum, item) => sum + item.data.reduce((acc, item) => acc + item?.orderCount, 0), 0)}</div>
+                    <div className='text-2xl text-slate-600'>{isLoading ? <div className='w-28'><Spinner variant='gradient' size='sm' color='primary' /></div> : commissionData.reduce((sum, item) => sum + item.data.reduce((acc, item) => acc + item?.orderCount, 0), 0)}</div>
                     <div className='text-sm text-slate-500'></div>
                 </div>
             </div>
--- src/pages/DashboardCEO/Components/ExpensesDetails.jsx
+++ src/pages/DashboardCEO/Components/ExpensesDetails.jsx
@@ -23,15 +23,15 @@ function ExpensesDetails({ expensesData, selectExpensesTypeFromChart }) {
         <div className="relative max-h-[400px] overflow-y-auto">
             <Table aria-label="รายการค่าใช้จ่าย" isHeaderSticky removeWrapper>
                 <TableHeader columns={tableColumns}>
-                    {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+                    {(column) => <TableColumn key={column.key} className='text-center'>{column.label}</TableColumn>}
                 </TableHeader>
                 <TableBody items={filterExpenses} emptyContent={<span>ไม่มีข้อมูล</span>}>
                     {(item) => (
                         <TableRow key={item.expensesId} className='h-12 border-b-1 border-slate-100 sticky bottom-0 bg-white'>
-                            <TableCell>{item?.expensesType?.typeName}</TableCell>
-                            <TableCell>{item?.remarks}</TableCell>
-                            <TableCell>{formatNumber(item?.totalAmount)}</TableCell>
-                            <TableCell>
+                            <TableCell className='text-center'>{item?.expensesType?.typeName}</TableCell>
+                            <TableCell className='text-center'>{item?.remarks}</TableCell>
+                            <TableCell className='text-center'>{formatNumber(item?.totalAmount)}</TableCell>
+                            <TableCell className='text-center'>
                                 {new Date(item?.createdDate).toLocaleDateString('th-GB', {
                                     day: '2-digit',
                                     month: '2-digit',
@@ -48,7 +48,10 @@ function ExpensesDetails({ expensesData, selectExpensesTypeFromChart }) {
             <div className="sticky bottom-0 border-t border-gray-200 z-10 bg-slate-50 shadow-md rounded-lg h-12 flex flex-row justify-center items-center">
                 <div className="flex flex-row justify-between items-center px-4 py-2 font-medium text-sm w-full px-12">
                     <div className="col-span-1">รวมทั้งหมด ({filterExpenses?.length} รายการ)</div>
-                    <div className="col-span-1 text-red-500">{formatNumber(totalAmount)}</div>
+                    <div className="col-span-1 space-x-2">
+                        <span className='text-red-500'>{formatNumber(totalAmount)}</span>
+                        <span className='text-sm text-slate-500'>บาท</span>
+                    </div>
                 </div>
             </div>
         </div>
