242ffb5c773ad5a2362f051d248aef3a22e1bd58
Nattawut2324 <natwtlim123@gmail.com>
2025-05-26 19:12:08 +0700

    Update AgentManagePage.jsx

--- src/pages/Management/Pages/AgentManage/AgentManagePage.jsx
+++ src/pages/Management/Pages/AgentManage/AgentManagePage.jsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, Tab, Table, TableBody, TableColumn, TableHeader, Tabs } from "@heroui/react";
+import { Card, CardBody, Tab, Tabs } from "@heroui/react";
 import { useState } from "react";
 import AgentAccessManagePage from "./subPages/AgentAccessManagePage";
 
