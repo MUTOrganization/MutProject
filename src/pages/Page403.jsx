822a33d3bbeb4e525861067c8354b814d7c5f183
YESTERD4Y <165787169+YESTERD4Y@users.noreply.github.com>
2025-06-09 20:07:15 +0700

    Update Page403.jsx

--- src/pages/Page403.jsx
+++ src/pages/Page403.jsx
@@ -1,20 +1,45 @@
-import { Lock } from "lucide-react";
+import { Button } from "@heroui/react";
+import { LockKeyhole } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Page403() {
+
+  const navigate = useNavigate();
+  const [counter, setCounter] = useState(10);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [counter]);
+
+  useEffect(() => {
+    if (counter === 0) {
+      navigate('/');
+    }
+  }, [counter]);
+
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-50">
       <div className="text-center">
         <div className="mb-4">
-          <Lock className="w-24 h-24 mx-auto text-red-500" />
+          <LockKeyhole className="w-24 h-24 mx-auto text-red-500" />
         </div>
         <h1 className="text-6xl font-bold text-gray-800 mb-4">403</h1>
         <p className="text-2xl text-gray-600 mb-8">คุณไม่มีสิทธิ์เข้าถึงหน้านี้</p>
-        <button 
-          onClick={() => window.history.back()} 
-          className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors"
+        <p className="text-xl text-red-500 font-bold mb-8">กำลังนำท่านกลับหน้าหลักใน {counter} วินาที</p>
+        <Button 
+          color="primary"
+          variant="solid"
+          className="text-sm font-medium shadow-md hover:scale-105 transition-transform"
+          onPress={() => navigate(-1)}
         >
           กลับไปหน้าก่อนหน้า
-        </button>
+        </Button>
       </div>
     </div>
   );
