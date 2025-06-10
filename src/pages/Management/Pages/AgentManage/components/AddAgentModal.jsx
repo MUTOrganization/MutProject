7fd2f959efc0e4e5fc280a60e7bb3751e852c4fa
YESTERD4Y <165787169+YESTERD4Y@users.noreply.github.com>
2025-06-10 21:33:36 +0700

    Update AddAgentModal.jsx

--- src/pages/Management/Pages/AgentManage/components/AddAgentModal.jsx
+++ src/pages/Management/Pages/AgentManage/components/AddAgentModal.jsx
@@ -38,9 +38,11 @@ function AddAgentModal({ isOpen, onClose, fetchAgentList }) {
             onClose()
             fetchAgentList()
         }).catch((err) => {
+            console.log(err.response.data.error)
             if (err.status === 400) {
-                setError('รหัสตัวแทนหรือชื่อตัวแทนนี้มีอยู่ในระบบแล้ว', 'กรุณากรอกรหัสตัวแทนและชื่อตัวแทนใหม่')
-                toastWarning('ไม่สามารถเพิ่มตัวแทนได้', 'รหัสตัวแทนหรือชื่อตัวแทนนี้มีอยู่ในระบบแล้ว')
+                const e = err.response.data.error;
+                setError('รหัสตัวแทนหรือชื่อตัวแทนนี้มีอยู่ในระบบแล้ว', e)
+                toastWarning('ไม่สามารถเพิ่มตัวแทนได้', e)
             } else {
                 toastError('ไม่สามารถเพิ่มตัวแทนได้', 'เนื่องจากมีข้อผิดพลาดทางระบบ')
             }
